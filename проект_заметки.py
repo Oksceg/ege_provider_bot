abc03f8589bc68e400df4ac811e2b2ce49152406
Oksceg <55767547+Oksceg@users.noreply.github.com>
2020-03-11 15:28:13 +0300

    Add files via upload

---
+++ проект_заметки.py
@@ -0,0 +1,5 @@
+import selenium
+driver = webdriver.Chrome()
+driver.get('https://canvas.case.edu')
+#1011156027:AAElJxSe9oD2-GNkMiBG5x8jEIQmFrJRZCY - код доступа к боту
+#https://habr.com/ru/post/448310/ - инструкция по созданию бота с помощью питона
