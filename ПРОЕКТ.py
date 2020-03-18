bb89d28deb52cd63b436424eab6cadb277b31442
Oksceg <55767547+Oksceg@users.noreply.github.com>
2020-03-18 21:43:57 +0300

    Update ПРОЕКТ.py

--- ПРОЕКТ.py
+++ ПРОЕКТ.py
@@ -1,2 +1,6 @@
-pip install pytelegrambotapi #библиотека для телеги, далее следует обращаться к BotFather
-import telebot
+import telebot #pip install pytelegrambotapi - библиотека для телеги, далее следует обращаться к BotFather
+bot = telebot.TeleBot('1011156027:AAElJxSe9oD2-GNkMiBG5x8jEIQmFrJRZCY')
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Привет, ты написал мне /start')
+bot.polling()
