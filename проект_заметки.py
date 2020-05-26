6aeac41bc7e76eb06e7351e2a4dfe5fe5a50ad39
Oksceg <55767547+Oksceg@users.noreply.github.com>
2020-05-26 19:59:03 +0300

    Delete проект_заметки.py

--- проект_заметки.py
+++
@@ -1,5 +0,0 @@
-import selenium #строчки 1-3: пример использования библиотеки selenium
-driver = webdriver.Chrome()
-driver.get('https://canvas.case.edu')
-#1011156027:AAElJxSe9oD2-GNkMiBG5x8jEIQmFrJRZCY - код доступа к боту
-#https://habr.com/ru/post/448310/ - инструкция по созданию бота с помощью питона
