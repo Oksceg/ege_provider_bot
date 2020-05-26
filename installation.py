4b3a1d0d38cebf6e6270118534d640049a00c213
Oksceg <55767547+Oksceg@users.noreply.github.com>
2020-05-26 21:07:58 +0300

    Update and rename ПРОЕКТ.py to installation.py

---
+++ installation.py
@@ -0,0 +1,11 @@
+'''Программа пишется и запускается в колабе.
+Часть 1: установка библиотек для работы с телеграмом, браузером, парсингом'''
+
+!pip install pytelegrambotapi
+!pip install pysocks
+!pip install schedule
+!pip install emoji
+!pip install selenium 
+!apt-get update # to update ubuntu to correctly run apt install
+!apt install chromium-chromedriver
+!cp /usr/lib/chromium-browser/chromedriver /usr/bin 
